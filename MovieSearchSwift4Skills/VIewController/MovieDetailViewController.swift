2a663a57869ff9c3358f244a621b1ff611fc2792
reich452 <reich452@regis.edu>
2018-02-16 21:33:46 -0700

    Added switch statment in detailTVC to update the popularity double to emoji stars

--- MovieSearchSwift4Skills/VIewController/MovieDetailViewController.swift
+++ MovieSearchSwift4Skills/VIewController/MovieDetailViewController.swift
@@ -21,10 +21,42 @@ class MovieDetailViewController: UIViewController {
     
     func updateViews() {
         guard let movie = movie else { return }
+        let popularity = Int(movie.popularity)
         titleLabel.text = movie.title
-        ratingLabel.text = "\(movie.popularity)"
+        ratingLabel.text = howManyStars(numberOfStars: popularity)
         overViewLabel.text = movie.overview
         posterImageView.loadImage(imagePath: movie.posterPath ?? "")
+    }
+    
+    private func howManyStars(numberOfStars: Int) -> String {
+        let starString: String
+        
+        switch numberOfStars {
+        case 1:
+            starString = "⭐️"
+        case 2:
+            starString = "⭐️⭐️"
+        case 3:
+            starString = "⭐️⭐️⭐️"
+        case 4:
+            starString = "⭐️⭐️⭐️⭐️"
+        case 5:
+            starString = "⭐️⭐️⭐️⭐️⭐️"
+        case 6:
+            starString = "⭐️⭐️⭐️⭐️⭐️⭐️"
+        case 7:
+            starString = "⭐️⭐️⭐️⭐️⭐️⭐️⭐️"
+        case 8:
+            starString = "⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️"
+        case 9:
+            starString = "⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️"
+        case 10:
+            starString = "⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️"
+        default:
+            starString = "Not enough ratings"
+        }
+        
+        return starString
     }
 
     @IBOutlet weak var posterImageView: UIImageView!

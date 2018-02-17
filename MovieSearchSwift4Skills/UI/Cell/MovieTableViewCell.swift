fa485a82b26724b0570ff68b37db38166d73f744
reich452 <reich452@regis.edu>
2018-02-16 18:14:15 -0700

    Added custom background UIVIew

---
+++ MovieSearchSwift4Skills/UI/Cell/MovieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieTableViewCell.swift
+//  MovieSearchSwift4Skills
+//
+//  Created by Nick Reichard on 2/16/18.
+//  Copyright © 2018 Nick Reichard. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

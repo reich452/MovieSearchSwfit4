5a4002280226b64653c66780de29fdb2a8ab3c46
reich452 <reich452@regis.edu>
2018-02-18 14:50:44 -0700

    Made file for array Helper extention

--- MovieSearchSwift4Skills/ModelController/MovieController.swift
+++ MovieSearchSwift4Skills/ModelController/MovieController.swift
@@ -153,18 +153,4 @@ class MovieController {
 }
 
 
-extension Array where Element:Equatable {
-    func removeDuplicates() -> [Element] {
-        var result = [Element]()
-        
-        for value in self {
-            if result.contains(value) == false {
-                result.append(value)
-            }
-        }
-        
-        return result
-    }
-}
-
 

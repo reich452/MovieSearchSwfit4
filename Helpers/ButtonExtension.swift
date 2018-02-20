60b811296a7f428171e459a7204966de90fc5fc5
reich452 <reich452@regis.edu>
2018-02-19 20:33:38 -0700

    Made the button animations less disruptive

--- Helpers/ButtonExtension.swift
+++ Helpers/ButtonExtension.swift
@@ -14,7 +14,7 @@ extension UIButton {
         let animation = CAKeyframeAnimation(keyPath: "transform.translation.y")
         animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
         animation.duration = 0.7
-        animation.values = [-20.0, 20.0, -20.0, 20.0, -15.0, 15.0, 0.0 ]
+        animation.values = [-20.0, 20.0, 0.0 ]
         layer.add(animation, forKey: "shake")
     }
     
@@ -22,7 +22,7 @@ extension UIButton {
         let animation = CAKeyframeAnimation(keyPath: "transform.translation.x")
         animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
         animation.duration = 0.7
-        animation.values = [-20.0, 20.0, -20.0, 20.0, -10.0, 10.0, -5.0, 5.0, 0.0 ]
+        animation.values = [-20.0, 20.0, -10.0, 10.0, -5.0, 5.0, 0.0 ]
         layer.add(animation, forKey: "shake")
     }
     

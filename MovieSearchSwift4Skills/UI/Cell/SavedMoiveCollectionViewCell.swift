2dea54d69cddc0a0605fea45dcf745c0ba317a24
reich452 <reich452@regis.edu>
2018-02-20 14:46:01 -0700

    Added SavedMovieVC and movieCollectionViewCell

---
+++ MovieSearchSwift4Skills/UI/Cell/SavedMoiveCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SavedMoiveCollectionViewCell.swift
+//  MovieSearchSwift4Skills
+//
+//  Created by Nick Reichard on 2/20/18.
+//  Copyright © 2018 Nick Reichard. All rights reserved.
+//
+
+import UIKit
+
+class SavedMoiveCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ MovieSearchSwift4Skills/VIewController/SavedMovieViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SavedMovieViewController.swift
+//  MovieSearchSwift4Skills
+//
+//  Created by Nick Reichard on 2/20/18.
+//  Copyright © 2018 Nick Reichard. All rights reserved.
+//
+
+import UIKit
+
+class SavedMovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
